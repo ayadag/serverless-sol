a459d445870e174c4a2fde24a0e3ecf5f9f4bab5
96ayad <96ayad@gmail.com>
2024-07-17 19:38:46 +0300

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
+
 module.exports = {
     async headers() {
       return [
         {
           // matching all API routes
-          source: "/(.*)",
+          source: "/api/:path*",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
             { key: "Access-Control-Allow-Origin", value: "*" },
